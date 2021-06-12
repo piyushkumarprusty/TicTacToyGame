d6f352d1a05f93f3fc48e3b9f19964c1f54e913b
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-06-12 13:29:15 +0530

    first design

---
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Buclick(View view) {
+        Button buSelected = (Button) view;
+        buSelected.setBackgroundColor(Color.RED);
+    }
+}
